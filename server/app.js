7e32523e12de5012f27d8be70ad0bc5aab4ce5f2
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-07-07 12:07:37 -0300

    Exercício 2 Página Inicial

--- server/app.js
+++ server/app.js
@@ -1,30 +1,41 @@
 // importação de dependência(s)
+import express from 'express';
+import fs from 'fs';
+const app = express();
+app.use( express.static(`client/`));
 
+app.listen(3000, () => {
+    console.log('Escutando em: http://localhost:3000')
+})
 
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
 
-
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
+let jogadores = JSON.parse(fs.readFileSync('server/data/jogadores.json', 'utf8'));
+let jogosPorJogador = JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json', 'utf8'));
 
-
-
+db.jogadores = jogadores;
+db.jogosPorJogador = jogosPorJogador;
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???qual-templating-engine???');
 //app.set('views', '???caminho-ate-pasta???');
 // dica: 2 linhas
-
+app.set('view engine', 'hbs');
+app.set('views', 'server/views');
 
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-
+app.get('/', (req, res) => {
+    res.render('index', { jogadores: db.jogadores });
+});
 
 
 // EXERCÍCIO 3
