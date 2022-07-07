85f68859b11370b40c0cca2a92b699c8b32fdcbc
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-07-07 14:46:53 -0300

    Atividade 3

--- server/app.js
+++ server/app.js
@@ -2,15 +2,21 @@
 import express from 'express';
 import fs from 'fs';
 const app = express();
-app.use( express.static(`client/`));
-
-app.listen(3000, () => {
-    console.log('Escutando em: http://localhost:3000')
-})
 
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
+
+// EXERCÍCIO 1
+// configurar para servir os arquivos estáticos da pasta "client"
+// dica: 1 linha de código
+app.use(express.static(`client/`));
+
+// abrir servidor na porta 3000 (constante PORT)
+// dica: 1-3 linhas de código
+app.listen(3000, () => {
+    console.log('Escutando em: http://localhost:3000')
+})
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
@@ -43,12 +49,31 @@ app.get('/', (req, res) => {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
+app.get('/jogador/:id', (req, res) => {
+    let id = req.params.id;
+    let jogador = db.jogadores.players.find(jogador => jogador.steamid == id);
+    let jogos = db.jogosPorJogador[id].games;
+    let qntdJogos = db.jogosPorJogador[id].game_count;
+    let naoJogou = jogos.reduce((acc, jog) => {
+        if (jog.playtime_forever === 0) {
+            acc.push(jog);
+        }
+        return acc;
+    },[]);
 
+    let top5Jogos = jogos.sort((a, b) => { return b.playtime_forever - a.playtime_forever; }).slice(0, 5);
+    
+    top5Jogos.map(jogo => { jogo.playtime_forever = Math.round(jogo.playtime_forever / 60); });
 
-// EXERCÍCIO 1
-// configurar para servir os arquivos estáticos da pasta "client"
-// dica: 1 linha de código
+    let jogoFavorito = top5Jogos[0];
 
+    res.render('jogador', {
+        jogador: jogador,
+        jogos: jogos,
+        qntdJogos: qntdJogos,
+        naoJogou: naoJogou.length,
+        top5Jogos: top5Jogos,
+        jogoFavorito: jogoFavorito,
+    });
 
-// abrir servidor na porta 3000 (constante PORT)
-// dica: 1-3 linhas de código
+});
